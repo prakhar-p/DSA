eef5d6d4dc3fa7fe83e5cbb5f2f35b1e92d357d3
prakhar-p <74608125+prakhar-p@users.noreply.github.com>
2021-01-30 10:56:53 +0530

    Add files via upload

---
+++ experiment-13.cpp
@@ -0,0 +1,29 @@
+//done by prakhar 196032
+
+#include<stdio.h>
+#include<conio.h>
+int main(){
+	int n;
+	scanf("%d",&n);
+	int arr[n];
+	for(int i=0;i<n;i++){
+		scanf("%d",&arr[i]);
+	}
+
+	printf("\n");
+	for(int i=1;i<n;i++){
+		int hand=arr[i];
+		int j=i-1;
+		while(j>=0 and hand<arr[j]){
+			arr[j+1]=arr[j];
+			j--;
+		}
+		arr[j+1]=hand;
+	}
+	for(int i=0;i<n;i++){
+		printf("%d ",arr[i]);
+	}
+	printf("\n");
+	printf("---------------------------\n");
+	printf("Done By Prakhar 196032");	
+}
